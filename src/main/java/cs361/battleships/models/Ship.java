6ba938905510b81ea2f3be674de199d31f42d041
Nolan Nichols <nicholsn@oregonstate.edu>
2019-01-22 19:35:12 -0800

    testing branch commit

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -9,16 +9,23 @@ public class Ship {
 
 	@JsonProperty private List<Square> occupiedSquares;
 
-	public Ship() {
+	//Constructor, called when init in main
+	public Ship(){
+
 		occupiedSquares = new ArrayList<>();
 	}
 	
 	public Ship(String kind) {
 		//TODO implement
+		//Take a name or model of a ship and return that string somewhere
+
 	}
 
 	public List<Square> getOccupiedSquares() {
 		//TODO implement
+		//Perhaps system.out.println() for debugging
+		//Could also provide a visual rep of occupied squares to the user
+		//Set the color of occupied squares to red
 		return null;
 	}
 }
